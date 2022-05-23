da0b3ff6237fc77cce755b4d3336509ff7a44bc1
Plane <PlaneGames@users.noreply.github.com>
2022-05-24 01:33:03 +0900

    1. Update Editor

--- Assets/Editor/HierarchyHighlight.cs
+++ Assets/Editor/HierarchyHighlight.cs
@@ -10,11 +10,13 @@ namespace Assets.Editor
     [InitializeOnLoad]
     public class HierarchyHighlightManager
     {
-        public static readonly Color DEFAULT_COLOR_HIERARCHY_SELECTED = new Color(0.243f, 0.4901f, 0.9058f, 1f);
-
+        static Texture2D texture;
+        static Texture2D texture2;
 
         static HierarchyHighlightManager()
         {
+            texture = AssetDatabase.LoadAssetAtPath ("Assets/folder.png", typeof(Texture2D)) as Texture2D;
+            texture2 = AssetDatabase.LoadAssetAtPath ("Assets/free_icon.png", typeof(Texture2D)) as Texture2D;
             EditorApplication.hierarchyWindowItemOnGUI -= HierarchyHighlight_OnGUI;
             EditorApplication.hierarchyWindowItemOnGUI += HierarchyHighlight_OnGUI;
         }
@@ -24,15 +26,67 @@ namespace Assets.Editor
         {
             GameObject _obj = EditorUtility.InstanceIDToObject(inSelectionID) as GameObject;
 
-            if (_obj != null)
+            if (_obj != null && HierarchyNameTagMgr.Name_Tag != null)
             {
-                if(_obj.name.Contains("@") && Event.current.type == EventType.Repaint)
+                Color BKCol = new Color(1f, 1f, 1f, 0.7f);
+                Color TextCol = new Color(1f, 1f, 1f, 0.7f);
+
+                for (int i = 0; i < HierarchyNameTagMgr.Name_Tag.Count; i ++)
+                {
+                    if (_obj.name.Contains(HierarchyNameTagMgr.Name_Tag[i].name))
+                    {
+                        BKCol = HierarchyNameTagMgr.Name_Tag[i].BG_col;
+                        TextCol = HierarchyNameTagMgr.Name_Tag[i].text_col;
+                        break;
+                    }
+                }
+
+                if (_obj.name.Contains("@") && Event.current.type == EventType.Repaint)
                 {
                     bool ObjectIsSelected = Selection.instanceIDs.Contains(inSelectionID);
 
-                    Color BKCol = HierarchyNameTagMgr.BG_COL;
-                    Color TextCol = HierarchyNameTagMgr.TEXT_COL;
+                    Rect _selection_rect = new Rect (inSelectionRect);
+                    _selection_rect.x = 0;
+                    _selection_rect.y = inSelectionRect.position.y;
+                    _selection_rect.width = GUILayoutUtility.GetLastRect().width;
 
+                    if (_selection_rect.Contains(Event.current.mousePosition))
+                    {
+                        Debug.Log(GUI.contentColor);
+                    }
+                    else
+                    {
+
+                    }
+
+                    Rect _panel = new Rect (inSelectionRect);
+                    _panel.x = 60;
+                    _panel.y = inSelectionRect.position.y;
+                    _panel.width = 16;
+                    _panel.height = 16;
+                    
+                    Color _prev_c = GUI.color;
+                    Color _cur_c = Color.white - _prev_c + new Color(0f, 0f, 0f, 1f);
+                    EditorGUI.DrawRect(_panel, _cur_c);
+                    
+                    if (ObjectIsSelected)
+                        _cur_c = GUI.skin.settings.selectionColor;
+                    else
+                        _cur_c = Color.white - _prev_c + new Color(0f, 0f, 0f, 1f);
+
+                    EditorGUI.DrawRect(_panel, _cur_c);
+
+
+                    Rect r = new Rect (inSelectionRect); 
+                    r.x = 58;
+                    r.y = inSelectionRect.position.y - 2.5f;
+                    r.width = 20;
+                    r.height = 20;
+
+                    GUI.color = _prev_c;
+                    GUI.Label (r, texture); 
+                    
+                    
                     if (!_obj.activeSelf)
                     {
                         TextCol = new Color(1f, 1f, 1f, 0.7f);
@@ -41,7 +95,6 @@ namespace Assets.Editor
                     FontStyle TextStyle = FontStyle.Normal;
 
                     Rect Offset = new Rect(inSelectionRect.position + new Vector2(18f, 0f), new Vector2(14f, 0f));
-
 
                     if (BKCol.a > 0f)
                     {
@@ -53,7 +106,6 @@ namespace Assets.Editor
                         else
                             EditorGUI.DrawRect(BackgroundOffset, BKCol);
                     }
-
 
                     EditorGUI.LabelField(Offset, "@", new GUIStyle()
                     {
--- Assets/Popups/HierarchyNameTagMgr.cs
+++ Assets/Popups/HierarchyNameTagMgr.cs
@@ -7,6 +7,7 @@ using UnityEditor;
 [Serializable]
 public struct GameObjectNameTag
 {
+    public string name;
     public Color BG_col;
     public Color text_col;
 }
@@ -14,21 +15,40 @@ public struct GameObjectNameTag
 [InitializeOnLoad]
 public class HierarchyNameTagMgr : MonoBehaviour
 {
+    [Header ("< Name Tag Setting >")]
     public Color BG_col   = new Color(0f, 0f, 0f, 0f);
     public Color text_col = new Color(1f, 0.729f, 0f, 1f);
-    public GameObjectNameTag name_tag;
+    public GameObjectNameTag[] name_tag;
 
+    public static List<GameObjectNameTag> Name_Tag;
     public static Color BG_COL, TEXT_COL;
 
     private void Awake() 
     {
+        SetNameTag();
         BG_COL = BG_col;
         TEXT_COL = text_col;
     }
     
     private void Update()
     {
+        SetNameTag();
         BG_COL = BG_col;
         TEXT_COL = text_col;
+    }
+
+    private void SetNameTag()
+    {
+        if (Name_Tag == null)
+        {
+            Name_Tag = new List<GameObjectNameTag>();
+            for (int i = 0; i < name_tag.Length; i ++)
+            {
+                if (!Name_Tag.Contains(name_tag[i]))
+                {
+                    Name_Tag.Add(name_tag[i]);
+                }
+            }
+        }
     }
 }
